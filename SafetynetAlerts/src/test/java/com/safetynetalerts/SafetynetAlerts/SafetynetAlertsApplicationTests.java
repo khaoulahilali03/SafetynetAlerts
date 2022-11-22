1a4d47b3d34c22ddb1fecbc7588581e1fbc8a924
Khaoula <hilali.khaoula@gmail.com>
2022-11-22 23:10:48 +0100

    The Safetynet Alerts project

---
+++ SafetynetAlerts/src/test/java/com/safetynetalerts/SafetynetAlerts/SafetynetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynetalerts.SafetynetAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
